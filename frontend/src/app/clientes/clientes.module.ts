f139e8a126c74d885806c77f9f3e26ba5011644d
MauroDegaspari <mauro_degaspari@hotmail.com>
2023-02-11 19:36:30 -0300

    frontend - criação de clienteForm componentes

--- frontend/src/app/clientes/clientes.module.ts
+++ frontend/src/app/clientes/clientes.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientesRoutingModule } from './clientes-routing.module';
+import { ClienteFormComponent } from './cliente-form/cliente-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ClienteFormComponent
+  ],
   imports: [
     CommonModule,
     ClientesRoutingModule
