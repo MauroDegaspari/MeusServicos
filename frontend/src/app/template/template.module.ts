d2ad0ab6ae44c84b0e567076dffbea0250cf1ba2
MauroDegaspari <mauro_degaspari@hotmail.com>
2023-02-18 22:05:04 -0300

    Frontend - ajuste em sidebar, routerModules & routerLinks

--- frontend/src/app/template/template.module.ts
+++ frontend/src/app/template/template.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component'
@@ -13,7 +14,8 @@ import { FooterComponent } from './footer/footer.component'
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     NavbarComponent,
