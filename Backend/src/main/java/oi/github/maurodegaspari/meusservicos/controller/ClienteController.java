4f26cda3de519c6d36df4e7f4728696e1f4c2b81
MauroDegaspari <mauro_degaspari@hotmail.com>
2023-04-07 13:39:32 -0300

    Backend - Liberação de Cors do contolle para testes

--- Backend/src/main/java/oi/github/maurodegaspari/meusservicos/controller/ClienteController.java
+++ Backend/src/main/java/oi/github/maurodegaspari/meusservicos/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@CrossOrigin("http://localhost:4200")
 								//RequestBody para indicar que virar um objeto Json da requisição 
 	public ClienteModel salvar(@RequestBody ClienteModel cliente) {
 		return clienteRepo.save(cliente);
